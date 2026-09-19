d1d1afd46ea3425699239036fdaf6c683c959dc3
klaxons1 <59658354+klaxons1@users.noreply.github.com>
2026-09-19 18:31:42 +0300

    mascotcapsule3d decomp

--- segarally/sources/p000/InterfaceC0009ar.java
+++
@@ -1,7 +0,0 @@
-package p000;
-
-/* JADX INFO: renamed from: ar */
-/* JADX INFO: loaded from: C:\Temp\jadx-13810906317745716458\classes.dex */
-public interface InterfaceC0009ar {
-    void Code(C0040z c0040z);
-}
